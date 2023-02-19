ab802601e3f15f18ca169c8c5c1b5fb9b2adf81b
Sanket P <sannket@live.com>
2023-02-19 15:29:02 +0530

    simple first

---
+++ app.py
@@ -0,0 +1,4 @@
+print("hello world sanket")
+ageyear = input("What is your birth year ?")
+myage = 2022 - int(ageyear)
+print("You are " + str(myage) + " old")
---
+++ sum.py
@@ -0,0 +1,4 @@
+print("Program will do sum of two numbers. Enter numbers below.")
+first = input("First:")
+second = input("Second:")
+print("Sum", str(float(first) + float(second)))
